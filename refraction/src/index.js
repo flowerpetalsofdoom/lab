3c86c447917ab127594eab0029de57ba86d796bc
atiqs <hello@atiqah.com>
2022-10-25 17:23:37 +0800

    Update text links

--- refraction/src/index.js
+++ refraction/src/index.js
@@ -6,12 +6,13 @@ import Logo from './img/flwrs-logo.png'
 function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      <a href="https://atiqah.com/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>flwrs —</div>
+      <a href="https://atiqah.com/" style={{ position: 'absolute', bottom: 58, left: 90, fontSize: '13px' }}>
         atiqah.com
-        <br />
+      </a>
+      <a href="https://www.instagram.com/flowerpetalsofdoom/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
         @flowerpetalsofdoom
       </a>
-      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>flwrs —</div>
       <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>25/10/2022</div>
     </div>
   )
